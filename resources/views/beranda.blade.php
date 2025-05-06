95a7246b5534840b28fa70ae132cce5d915614b4
ackmad <ackmadelfanp@gmail.com>
2025-05-06 08:54:07 +0700

    FAN : Menambah dan Mengembangkan Landing Page

---
+++ resources/views/beranda.blade.php
@@ -0,0 +1,108 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>ELFAN</title>
+    <link rel="stylesheet" href="{{ asset('css/style_beranda.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/fonts.css') }}">
+</head>
+<body>
+    <div class="container">
+        <div id="navbar">
+            <ul>
+                <li><a href="#">Rumah</a></li>
+                <li><a href="#">Curhat Dulu yuk</a></li>
+                <li><a href="#">Intip Cerita Orang</a></li>
+                <li><a href="#">Kenali Kami</a></li>
+                <li><a href="#">Ada Saran?</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="container" style="padding-top: 10%">
+        <h1 class="tagline-home">Tempat Curhat Tanpa Nama</h1>
+        <h1 class="tagline-home" style="font-size: 40px">Tapi Tetap Didengar</h1>
+        <h4>“Curhat itu bukan drama. Itu bagian dari manusia.”</h4>
+        <div class="calltoaction">
+            <a href="#" class="btn-curhat">📝 Curhat Sekarang</a>
+            <a href="#" class="btn-bacaCurhat">📖 Baca Cerita Orang</a>
+        </div>
+    </div>
+    <div class="section-kategori">
+        <div class="left-side">
+            <p>"Nggak semua luka harus terlihat, tapi semuanya layak didengar</p>
+            <div class="kategori-side">
+                <h4>Tampilkan Kategori: </h4>
+                <div class="kategori">
+                    <a href="#" class="btn-kategori">Lagi Urusan Hati</a>
+                    <a href="#" class="btn-kategori">Cerita dari Rumah</a>
+                    <a href="#" class="btn-kategori">Tentang Temen-Temen</a>
+                    <a href="#" class="btn-kategori">Perang Sama Diri Sendiri</a>
+                    <a href="#" class="btn-kategori">Pelan-Pelan, Nggak Apa-Apa</a>
+                    <a href="#" class="btn-kategori">Masa Depan Gimana Ya?</a>
+                    <a href="#" class="btn-kategori">Dompet Lagi Tipis?</a>
+                    <a href="#" class="btn-kategori">Suasana Sekitar Gue</a>
+                    <a href="#" class="btn-kategori">Cerita Hati & Tuhan</a>
+                    <a href="#" class="btn-kategori">Belajar Sayang Diri Sendiri</a>
+                    <a href="#" class="btn-kategori">Campur Aduk Aja deh</a>
+                </div>
+            </div>
+        </div>
+        <div class="right-side">
+            <div>
+                <img src="{{ asset('images/quotepapper.png') }}" alt="" width="600px">
+            </div>
+        </div>
+    </div>
+    <div class="container-postByCategory">
+        <div class="postCard-item">
+            <p>22.01.08</p>
+            <div class="content">
+                <h3>Ternyata cinta jarak jauh itu nggak sesimpel katanya sabar</h3>
+                <p>Kita udah sama-sama tahu ini nggak bakal gampang, tapi akhir-akhir ini aku ngerasa kayak jalan sendiri.</p>
+            </div>
+            <p>Cinta</p>
+        </div>
+        <div class="postCard-item">
+            <p>22.01.08</p>
+            <div class="content">
+                <h3>Ternyata cinta jarak jauh itu nggak sesimpel katanya sabar</h3>
+                <p>Kita udah sama-sama tahu ini nggak bakal gampang, tapi akhir-akhir ini aku ngerasa kayak jalan sendiri.</p>
+            </div>
+            <p>Cinta</p>
+        </div>
+        <div class="postCard-item">
+            <p>22.01.08</p>
+            <div class="content">
+                <h3>Ternyata cinta jarak jauh itu nggak sesimpel katanya sabar</h3>
+                <p>Kita udah sama-sama tahu ini nggak bakal gampang, tapi akhir-akhir ini aku ngerasa kayak jalan sendiri.</p>
+            </div>
+            <p>Cinta</p>
+        </div>
+        <div class="postCard-item">
+            <p>22.01.08</p>
+            <div class="content">
+                <h3>Ternyata cinta jarak jauh itu nggak sesimpel katanya sabar</h3>
+                <p>Kita udah sama-sama tahu ini nggak bakal gampang, tapi akhir-akhir ini aku ngerasa kayak jalan sendiri.</p>
+            </div>
+            <p>Cinta</p>
+        </div>
+        <div class="postCard-item">
+            <p>22.01.08</p>
+            <div class="content">
+                <h3>Ternyata cinta jarak jauh itu nggak sesimpel katanya sabar</h3>
+                <p>Kita udah sama-sama tahu ini nggak bakal gampang, tapi akhir-akhir ini aku ngerasa kayak jalan sendiri.</p>
+            </div>
+            <p>Cinta</p>
+        </div>
+    </div>
+    <div class="footer">
+        <h3>Kita semua pernah ngerasa sendirian, ngerasa nggak didengerin, atau sekadar pengen cerita tanpa takut dihakimi. Di sinilah Curhat.id berdiri — bukan buat sok tau, tapi buat nemenin kamu yang mungkin lagi bingung, sedih, jatuh cinta diam-diam, atau bahkan... gagal move on padahal udah berdoa tiap malam. 😌</h1>
+
+        <p>Curhat.id dibangun dari keresahan—iya, keresahan yang seringnya nggak bisa kita ucapin di dunia nyata. Tapi tenang, di sini kamu bebas. Mau cerita panjang, pendek, acak-acakan, atau typo segudang, kita baca satu per satu (dan diem-diem baper juga kadang 🥹).</p>
+
+        <h5>© 2025 Curhat.id | Made with kopi, coding, dan cerita-cerita larut malam.</h5>
+    </div>
+</body>
+</html>
